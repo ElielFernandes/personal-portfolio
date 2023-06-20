9dc87bd7a3d3769d5c71c75c3e1950161a01057d
Eliel Fernandes <elieljacomit@gmail.com>
2023-06-20 17:09:04 -0300

    feat: links to github and linkedin

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,8 @@ import { Canvas } from "../../components/Canvas";
 import foto from "../../images/eliel.png";
 import { Card } from "../../components/Card";
 
+import { BsLinkedin, BsGithub} from 'react-icons/bs';
+
 export const Home = () => {
     return (
         <Canvas>
@@ -14,6 +16,14 @@ export const Home = () => {
                         <div className="home-info-hello">Olá!</div>
                         <div className="home-info-name">Meu nome é Eliel</div>
                         <div className="home-info-web">Desenvolvedor Back End</div>
+                        <div className="home-info-link">
+                            <a className="home-info-link-github" href="https://github.com/ElielFernandes" target="_blank">
+                                <BsGithub className="home-info-icon-github" />
+                            </a> 
+                            <a className="home-info-link-linkdin" href="https://www.linkedin.com/in/eliel-jacomit-fernandes-0bba10175/" target="_blank">
+                                <BsLinkedin className="home-info-icon-linkdin"/>
+                            </a> 
+                        </div>
                     </div>
                 </Card>
                 <Card>
--- src/pages/Home/styled.tsx
+++ src/pages/Home/styled.tsx
@@ -2,18 +2,11 @@ import styled from "styled-components";
 
 export const HomeStyle = styled.div`
 
-
-
     max-width: 1200px;
     margin: 0px auto;
     padding: 1rem;
     display: flex;
     height: calc(100% - 60px);
-
-
-    @media (max-width: 600px) { 
-        flex-direction: column;
-    }
 
     .home{
         display: flex;
@@ -57,5 +50,50 @@ export const HomeStyle = styled.div`
     .home-info-web{
         font-size: 1.3rem;
         color: rgb(0,0,0);
+        margin-bottom: 0.7rem;
+    }
+
+    .home-info-link{
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+
+        .home-info-link-github{
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 0.3rem;
+            text-decoration: none;
+            color: black;
+
+            .home-info-icon-github{
+                font-size: 2.2rem;
+    
+            }
+        }
+    
+        .home-info-link-linkdin{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 0.3rem;
+            text-decoration: none;
+            color: black;
+
+            .home-info-icon-linkdin{
+                font-size: 2.2rem;
+            }
+        }
+    }
+
+    @media (max-width: 600px) { 
+        flex-direction: column;
+
+        .home-info-link{
+            justify-content: center;
+        }
+
     }
 `;
